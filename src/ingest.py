bae10ebdf86450ae304e8dd65b5568fe2ea62f54
Joshua Koh <Joshua@kohfamily.com>
2017-11-28 17:24:46 -0500

    Convert to PyCharm

--- src/ingest.py
+++ src/ingest.py
@@ -7,4 +7,3 @@ df = pd.read_csv('data/college-scorecard.csv', sep=',')
 # Remove sufficiently null columns
 nullCols = df.columns[df.isnull().sum() > (len(df)/2)].tolist()
 df.drop(nullCols, axis=1, inplace=True)
-
