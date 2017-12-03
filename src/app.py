59936f3abc74da23799825600adc810571344ddd
Joshua Koh <Joshua@kohfamily.com>
2017-12-03 17:26:11 -0500

    Successful query by state!

--- src/app.py
+++ src/app.py
@@ -1,20 +1,30 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, session, render_template, request
+import os
+import pandas as pd
+import ingest as data
 
 app = Flask(__name__)
+df = pd.read_csv('../data/college-scorecard.csv', sep=',')
+
 
 @app.route('/')
 @app.route('/form')
 def form():
     return render_template('form.html',
                            title='Form Empty',
-                           message=None)
+                           result=None)
 
-@app.route('/form', methods=['GET', 'POST'])
+
+@app.route('/form', methods=['POST'])
 def form_submit():
+    state = request.form['state']
+    stateCount = data.getByState(state)
+
     return render_template('form.html',
                            title='Form Submitted',
-                           message="Success!")
+                           result=stateCount)
+
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
     app.run()
--- src/ingest.py
+++ src/ingest.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import numpy as np
+from app import df
+
 
 # Read and clean data
-df = pd.read_csv('data/college-scorecard.csv', sep=',')
+def importAndPrep(app):
+    # Remove sufficiently null columns
+    nullCols = df.columns[df.isnull().sum() > (len(df)/2)].tolist()
+    df.drop(nullCols, axis=1, inplace=True)
 
-# Remove sufficiently null columns
-nullCols = df.columns[df.isnull().sum() > (len(df)/2)].tolist()
-df.drop(nullCols, axis=1, inplace=True)
+    debug = "Prepared %i rows!" % len(df)
+    app.logger.debug(debug)
+
+    return df
+
+
+def getByState(state):
+    print(df.head())
+    return len(df[df.STABBR == state])
